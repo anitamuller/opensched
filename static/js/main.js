0f699d2ef6833135899e17ea82e531dee3b6a782
Anita <ani.mullerr@gmail.com>
2014-08-03 17:58:47 -0300

    - intentando agregar control select all checkbox (q por ahora no me da bola) en /event_permalink/talks

--- static/js/main.js
+++ static/js/main.js
@@ -34,7 +34,17 @@ $(function(){
     $('#talk-end').timepicker();
 
 
-
+    $('#select-all').on('click', function() {  //on click
+        if(this.checked) { // check select status
+            $('.delete-talk').each(function() { //loop through each checkbox
+                this.checked = true;  //select all checkboxes with class "delete-talk_checkbox"
+            });
+            }else{
+                $('.delete-talk').each(function() { //loop through each checkbox
+                    this.checked = false; //deselect all checkboxes with class "delete-talk_checkbox"
+                });
+                }
+    });
 
 
 });
