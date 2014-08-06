610d5cc6b0bfb54ef969595e7f742a5d16247b30
Victoria Martínez de la Cruz <victoria@vmartinezdelacruz.com>
2014-08-06 12:02:22 -0300

    Agrego summernote, editor WYSIWYG de bootstrap

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,78 @@
 window.MDM_SILENT = true;
-$(function(){
+$(function() {
     $('a[data-target="_blank"]').attr('target', '_blank');
 
-    if($('#event-summary').length && $('#event-description').length) {
-         $('#event-summary').mdmagick();
-         $('#event-description').mdmagick();
+    if ($('#event-summary').length && $('#event-description').length) {
+        $('#event-summary').summernote({
+            height: 150,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+            ]
+        });
+        $('#event-description').summernote({
+            height: 150,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+            ]
+        });
     }
 
-    if($('#talk-summary').length && $('#talk-description').length) {
-        $('#talk-summary').mdmagick();
-        $('#talk-description').mdmagick();
+    if ($('#talk-summary').length && $('#talk-description').length) {
+        $('#talk-summary').summernote({
+            height: 150,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+            ]
+        });
+        $('#talk-description').summernote({
+            height: 150,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+            ]
+        });
     }
 
-    $('#event-preview').on('click', function(){
-         var eventForm = $('#event-form');
-         eventForm.find('#preview').val('1');
-     });
-     $('#event-submit').on('click', function(){
-         var eventForm = $('#event-form');
-         eventForm.find('#preview').val('');
-     });
+    $('#event-preview').on('click', function() {
+        var eventForm = $('#event-form');
+        eventForm.find('#preview').val('1');
+    });
+    $('#event-submit').on('click', function() {
+        var eventForm = $('#event-form');
+        eventForm.find('#preview').val('');
+    });
 
-     $('#talk-preview').on('click', function(){
-         var talkForm = $('#talk-form');
-         talkForm.find('#preview').val('1');
-     });
+    $('#talk-preview').on('click', function() {
+        var talkForm = $('#talk-form');
+        talkForm.find('#preview').val('1');
+    });
 
-     $('#talk-submit').on('click', function(){
-         var talkForm = $('#talk-form');
-         talkForm.find('#preview').val('');
-     });
+    $('#talk-submit').on('click', function() {
+        var talkForm = $('#talk-form');
+        talkForm.find('#preview').val('');
+    });
 
 
-    $('a.icon').on('click', function(){
+    $('a.icon').on('click', function() {
         return confirm('Are you sure?');
     });
 });
-
-
